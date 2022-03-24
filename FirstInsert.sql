4f91017536fbacd3be21ea2aa6c61bcbb062c358
NoamShveber <62326952+NoamShveber@users.noreply.github.com>
2022-03-24 17:53:22 +0200

    updated files

---
+++ FirstInsert.sql
@@ -0,0 +1,280 @@
+insert into EMPLOYEE (ID, NAME, ADDRESS, PHONE, SALARY, BANKDETAIL, ROLE, EMAIL)
+values (583990715, 'Selma Crewson', '4 Buscemi Street', '0554435526', 12263, '50697', 'americium', 'selma@dillards.fi');
+
+insert into EMPLOYEE (ID, NAME, ADDRESS, PHONE, SALARY, BANKDETAIL, ROLE, EMAIL)
+values (523624920, 'Leelee Nolte', '271 Spacey Street', '0519136706', 141084, '83426', 'strontium', 'leelee.nolte@sht.de');
+
+insert into EMPLOYEE (ID, NAME, ADDRESS, PHONE, SALARY, BANKDETAIL, ROLE, EMAIL)
+values (158387926, 'Rebecca Imperioli', '566 Kutcher Ave', '0578927687', 54164, '70834', 'osmium', 'rebeccai@shar.com');
+
+insert into EMPLOYEE (ID, NAME, PHONE, EMAIL, BANKDETAIL, ADDRESS, SALARY, ROLE)
+values (230927098, 'Delroy Sinise', '0543560228', 'delroy.s@balchem.com', '26894', '39 Warwick', 2000, 'addddd');
+
+insert into EMPLOYEE (ID, NAME, PHONE, EMAIL, BANKDETAIL, ADDRESS, SALARY, ROLE)
+values (673197487, 'Malcolm Peet', '0594606851', 'malcolm.peet@technica.com', '13336', '36 Gothenburg Street', 1000, 'asndan');
+
+insert into PROFEMP (ID)
+values (583990715);
+
+insert into PROFEMP (ID)
+values (523624920);
+
+insert into PROFEMP (ID)
+values (158387926);
+
+insert into RESIDENT (ID, NAME, ADDRESS, PHONE, EMAIL, MAIL)
+values (583990715, 'Selma Crewson', '4 Buscemi Street', '0554435526', 'selma@dillards.fi', '78978');
+
+insert into RESIDENT (ID, NAME, ADDRESS, PHONE, EMAIL, MAIL)
+values (523624920, 'Leelee Nolte', '271 Spacey Street', '0519136706', 'leelee.nolte@sht.de', '12345');
+
+insert into RESIDENT (ID, NAME, PHONE, EMAIL, MAIL, ADDRESS)
+values (230927098, 'Delroy Sinise', '0543560228', 'delroy.s@balchem.com', '26894', '39 Warwick');
+
+insert into RESIDENT (ID, NAME, PHONE, EMAIL, MAIL, ADDRESS)
+values (673197487, 'Malcolm Peet', '0594606851', 'malcolm.peet@technica.com', '13336', '36 Gothenburg Street');
+
+INSERT INTO Budget(year)
+values (2019);
+
+INSERT INTO Budget(year)
+values (2020);
+
+INSERT INTO Budget(year)
+values (2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (3086, to_date('17-09-2020', 'dd-mm-yyyy'), 1, 2019);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (2522, to_date('15-08-2021', 'dd-mm-yyyy'), 2, 2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (3760, to_date('22-04-2019', 'dd-mm-yyyy'), 3, 2019);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (1337, to_date('09-05-2021', 'dd-mm-yyyy'), 4, 2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (2270, to_date('26-07-2021', 'dd-mm-yyyy'), 5, 2019);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (2815, to_date('05-04-2021', 'dd-mm-yyyy'), 6, 2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (2030, to_date('23-11-2020', 'dd-mm-yyyy'), 7, 2020);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (3080, to_date('21-07-2020', 'dd-mm-yyyy'), 8, 2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (1835, to_date('27-10-2021', 'dd-mm-yyyy'), 9, 2020);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (2803, to_date('21-07-2021', 'dd-mm-yyyy'), 10, 2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (2426, to_date('18-10-2021', 'dd-mm-yyyy'), 11, 2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (1661, to_date('30-12-2019', 'dd-mm-yyyy'), 12, 2020);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (1771, to_date('29-04-2021', 'dd-mm-yyyy'), 13, 2019);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (3095, to_date('17-05-2021', 'dd-mm-yyyy'), 14, 2020);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (1226, to_date('18-11-2020', 'dd-mm-yyyy'), 15, 2019);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (2841, to_date('05-02-2020', 'dd-mm-yyyy'), 16, 2020);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (2819, to_date('20-08-2021', 'dd-mm-yyyy'), 17, 2020);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (3542, to_date('04-04-2021', 'dd-mm-yyyy'), 18, 2019);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (2803, to_date('31-08-2021', 'dd-mm-yyyy'), 19, 2020);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (3803, to_date('05-06-2021', 'dd-mm-yyyy'), 20, 2020);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (1754, to_date('11-11-2020', 'dd-mm-yyyy'), 21, 2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (1649, to_date('23-05-2019', 'dd-mm-yyyy'), 22, 2019);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (2200, to_date('07-05-2020', 'dd-mm-yyyy'), 23, 2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (1011, to_date('21-06-2021', 'dd-mm-yyyy'), 24, 2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (2049, to_date('24-08-2020', 'dd-mm-yyyy'), 25, 2019);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (1888, to_date('09-02-2020', 'dd-mm-yyyy'), 26, 2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (1378, to_date('27-03-2021', 'dd-mm-yyyy'), 27, 2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (3947, to_date('05-09-2019', 'dd-mm-yyyy'), 28, 2020);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (1307, to_date('16-01-2019', 'dd-mm-yyyy'), 29, 2021);
+
+insert into SOURCE (AMOUNT, SDATE, SID, YEAR)
+values (3516, to_date('09-12-2021', 'dd-mm-yyyy'), 30, 2019);
+
+insert into collection (sID, ID)
+values (1, 230927098);
+
+insert into collection (sID, ID)
+values (2, 123456789);
+
+insert into collection (sID, ID)
+values (3, 230927098);
+
+insert into collection (sID, ID)
+values (4, 123456789);
+
+insert into collection (sID, ID)
+values (5, 230927098);
+
+insert into collection (sID, ID)
+values (6, 123456789);
+
+insert into GovFund (sID)
+values (7);
+
+insert into GovFund (sID)
+values (8);
+
+insert into GovFund (sID)
+values (9);
+
+insert into taxes (sID, type)
+values (1, 'a');
+
+insert into taxes (sID, type)
+values (2, 'b');
+
+insert into taxes (sID, type)
+values (3, 'c');
+
+insert into DONOR(ID, name)
+values (123456789,'asdfg');
+
+insert into DONOR(ID, name)
+values (987654321,'kjhgfds');
+
+insert into DONOR(ID, name)
+values (987612345,'qwertyui');
+
+insert into donation(sID, dID)
+values(10,123456789);
+
+insert into donation(sID, dID)
+values (11,987654321);
+
+insert into donation(sID, dID)
+values(12,987612345);
+
+insert into Inspector(ID)
+values (583990715);
+
+insert into Inspector(ID)
+values (523624920);
+
+insert into Inspector(ID)
+values (158387926);
+
+insert into Fines(reason, sID , iID)
+values ('abc', 4, 583990715);
+
+insert into Fines(reason, sID , iID)
+values ('def', 5, 583990715);
+
+insert into Fines(reason, sID , iID)
+values ('ghi', 6, 583990715);
+
+INSERT INTO PolEmp (PARTY, ID)
+values ('Likud', 523624920);
+
+INSERT INTO PolEmp (PARTY, ID)
+values ('Yesh Atid', 230927098);
+
+INSERT INTO PolEmp (PARTY, ID)
+values ('Likud', 673197487);
+
+insert into Member
+values (523624920);
+
+insert into Member
+values (230927098);
+
+insert into Member 
+values (673197487);
+
+insert into Chairman
+values (523624920);
+
+insert into Chairman
+values (230927098);
+
+insert into chairman
+values (673197487);
+
+insert into Committee values ('H', 'HSA', 523624920);
+insert into Committee values ('T', 'TSA', 230927098);
+insert into Committee values ('B', 'BSA', 673197487);
+
+insert into Institution(Address, instName, Type, Commitname)
+values('lkjhgfdsdfghjk','iuytre','poiuytre','H');
+insert into Institution(Address, instName, Type, Commitname)
+values('jhgfdx','iuytre','poiuytre','T');
+insert into Institution(Address, instName, Type, Commitname)
+values('ertyuio','iuytre','poiuytre','B');
+
+insert into MemberAt(Name, ID) values('H', 673197487);
+insert into MemberAt(Name, ID) values('H', 230927098);
+
+insert into MemberAt(Name, ID) values('T', 523624920);
+insert into MemberAt(Name, ID) values('T', 673197487);
+
+insert into MemberAt(Name, ID) values('B', 523624920);
+insert into MemberAt(Name, ID) values('B', 230927098);
+
+
+insert into Inquiries(iID, iDate, type, details, status, ID, commitName)
+values(1, to_date('15-01-2019', 'dd-mm-yyyy'), 'abc', 'jasdakbsdjkdsah', 'f', 523624920, 'H');
+
+insert into Inquiries(iID, iDate, type, details, status, ID, commitName)
+values(2, to_date('15-01-2019', 'dd-mm-yyyy'), 'def', 'fjesnfkjhfn sadk;lk', 'o', 523624920, 'B');
+
+insert into Inquiries(iID, iDate, type, details, status, ID, commitName)
+values(3, to_date('15-01-2019', 'dd-mm-yyyy'), 'ghi', 'alkndjew dj nja', 'i', 523624920, 'T');
+
+insert into Finance(year, name, amount) values(2019, 'H', 123);
+insert into Finance(year, name, amount) values(2020, 'H', 123);
+insert into Finance(year, name, amount) values(2021, 'H', 123);
+
+insert into Finance(year, name, amount) values(2019, 'T', 123);
+insert into Finance(year, name, amount) values(2020, 'T', 123);
+insert into Finance(year, name, amount) values(2021, 'T', 123);
+
+insert into Finance(year, name, amount) values(2019, 'B', 123);
+insert into Finance(year, name, amount) values(2020, 'B', 123);
+insert into Finance(year, name, amount) values(2021, 'B', 123);
+
+
+
+
+commit;
---
+++ FirstSelectAll.sql
@@ -0,0 +1,39 @@
+SELECT * FROM Employee;
+
+SELECT * FROM profEmp;
+
+SELECT * FROM Resident;
+
+SELECT * FROM polEmp;
+
+SELECT * FROM Member;
+
+SELECT * FROM Chairman;
+
+SELECT * FROM Budget;
+
+SELECT * FROM Source;
+
+SELECT * FROM Collection;
+
+SELECT * FROM GovFund;
+
+SELECT * FROM Taxes;
+
+SELECT * FROM Donor;
+
+SELECT * FROM Inspector;
+
+SELECT * FROM Committee;
+
+SELECT * FROM Institution;
+
+SELECT * FROM Donation;
+
+SELECT * FROM Fines;
+
+SELECT * FROM Inquiries;
+
+SELECT * FROM memberAt;
+
+SELECT * FROM Finance;

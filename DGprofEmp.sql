f4f2e3c17843b9340b8c3bbff5c10a209d4b0be6
nnaiman <nnaiman@g.jct.ac.il>
2022-03-28 19:07:39 +0300

    DG

---
+++ DGprofEmp.sql
@@ -0,0 +1,902 @@
+insert into SYSTEM.PROFEMP (ID)
+values (842129786);
+
+insert into SYSTEM.PROFEMP (ID)
+values (353902855);
+
+insert into SYSTEM.PROFEMP (ID)
+values (993643670);
+
+insert into SYSTEM.PROFEMP (ID)
+values (820692254);
+
+insert into SYSTEM.PROFEMP (ID)
+values (458733062);
+
+insert into SYSTEM.PROFEMP (ID)
+values (430862370);
+
+insert into SYSTEM.PROFEMP (ID)
+values (260087524);
+
+insert into SYSTEM.PROFEMP (ID)
+values (777069441);
+
+insert into SYSTEM.PROFEMP (ID)
+values (500023273);
+
+insert into SYSTEM.PROFEMP (ID)
+values (464352626);
+
+insert into SYSTEM.PROFEMP (ID)
+values (353842421);
+
+insert into SYSTEM.PROFEMP (ID)
+values (710071566);
+
+insert into SYSTEM.PROFEMP (ID)
+values (109777451);
+
+insert into SYSTEM.PROFEMP (ID)
+values (143199959);
+
+insert into SYSTEM.PROFEMP (ID)
+values (595158503);
+
+insert into SYSTEM.PROFEMP (ID)
+values (774793226);
+
+insert into SYSTEM.PROFEMP (ID)
+values (970470592);
+
+insert into SYSTEM.PROFEMP (ID)
+values (992350784);
+
+insert into SYSTEM.PROFEMP (ID)
+values (351422867);
+
+insert into SYSTEM.PROFEMP (ID)
+values (582374128);
+
+insert into SYSTEM.PROFEMP (ID)
+values (365071065);
+
+insert into SYSTEM.PROFEMP (ID)
+values (365071065);
+
+insert into SYSTEM.PROFEMP (ID)
+values (939417397);
+
+insert into SYSTEM.PROFEMP (ID)
+values (398811519);
+
+insert into SYSTEM.PROFEMP (ID)
+values (476898036);
+
+insert into SYSTEM.PROFEMP (ID)
+values (212075144);
+
+insert into SYSTEM.PROFEMP (ID)
+values (141406097);
+
+insert into SYSTEM.PROFEMP (ID)
+values (838448556);
+
+insert into SYSTEM.PROFEMP (ID)
+values (534510701);
+
+insert into SYSTEM.PROFEMP (ID)
+values (524704615);
+
+insert into SYSTEM.PROFEMP (ID)
+values (889007321);
+
+insert into SYSTEM.PROFEMP (ID)
+values (277323069);
+
+insert into SYSTEM.PROFEMP (ID)
+values (513050481);
+
+insert into SYSTEM.PROFEMP (ID)
+values (429070833);
+
+insert into SYSTEM.PROFEMP (ID)
+values (554353039);
+
+insert into SYSTEM.PROFEMP (ID)
+values (757309441);
+
+insert into SYSTEM.PROFEMP (ID)
+values (973165679);
+
+insert into SYSTEM.PROFEMP (ID)
+values (478845167);
+
+insert into SYSTEM.PROFEMP (ID)
+values (149349845);
+
+insert into SYSTEM.PROFEMP (ID)
+values (175230934);
+
+insert into SYSTEM.PROFEMP (ID)
+values (103793825);
+
+insert into SYSTEM.PROFEMP (ID)
+values (134623573);
+
+insert into SYSTEM.PROFEMP (ID)
+values (124556256);
+
+insert into SYSTEM.PROFEMP (ID)
+values (639368802);
+
+insert into SYSTEM.PROFEMP (ID)
+values (863264330);
+
+insert into SYSTEM.PROFEMP (ID)
+values (677729758);
+
+insert into SYSTEM.PROFEMP (ID)
+values (615738277);
+
+insert into SYSTEM.PROFEMP (ID)
+values (125994217);
+
+insert into SYSTEM.PROFEMP (ID)
+values (467277244);
+
+insert into SYSTEM.PROFEMP (ID)
+values (458733062);
+
+insert into SYSTEM.PROFEMP (ID)
+values (551897883);
+
+insert into SYSTEM.PROFEMP (ID)
+values (468456810);
+
+insert into SYSTEM.PROFEMP (ID)
+values (742907963);
+
+insert into SYSTEM.PROFEMP (ID)
+values (431404507);
+
+insert into SYSTEM.PROFEMP (ID)
+values (500955521);
+
+insert into SYSTEM.PROFEMP (ID)
+values (748587451);
+
+insert into SYSTEM.PROFEMP (ID)
+values (930705389);
+
+insert into SYSTEM.PROFEMP (ID)
+values (933001339);
+
+insert into SYSTEM.PROFEMP (ID)
+values (465852478);
+
+insert into SYSTEM.PROFEMP (ID)
+values (196623412);
+
+insert into SYSTEM.PROFEMP (ID)
+values (875952935);
+
+insert into SYSTEM.PROFEMP (ID)
+values (172660024);
+
+insert into SYSTEM.PROFEMP (ID)
+values (636754601);
+
+insert into SYSTEM.PROFEMP (ID)
+values (984556541);
+
+insert into SYSTEM.PROFEMP (ID)
+values (425034915);
+
+insert into SYSTEM.PROFEMP (ID)
+values (865427923);
+
+insert into SYSTEM.PROFEMP (ID)
+values (957209600);
+
+insert into SYSTEM.PROFEMP (ID)
+values (951460413);
+
+insert into SYSTEM.PROFEMP (ID)
+values (464270292);
+
+insert into SYSTEM.PROFEMP (ID)
+values (974444033);
+
+insert into SYSTEM.PROFEMP (ID)
+values (984730264);
+
+insert into SYSTEM.PROFEMP (ID)
+values (402713784);
+
+insert into SYSTEM.PROFEMP (ID)
+values (283602199);
+
+insert into SYSTEM.PROFEMP (ID)
+values (250619323);
+
+insert into SYSTEM.PROFEMP (ID)
+values (304636867);
+
+insert into SYSTEM.PROFEMP (ID)
+values (822374660);
+
+insert into SYSTEM.PROFEMP (ID)
+values (690861657);
+
+insert into SYSTEM.PROFEMP (ID)
+values (525730004);
+
+insert into SYSTEM.PROFEMP (ID)
+values (963561590);
+
+insert into SYSTEM.PROFEMP (ID)
+values (473281071);
+
+insert into SYSTEM.PROFEMP (ID)
+values (639368802);
+
+insert into SYSTEM.PROFEMP (ID)
+values (238369139);
+
+insert into SYSTEM.PROFEMP (ID)
+values (921497430);
+
+insert into SYSTEM.PROFEMP (ID)
+values (353902855);
+
+insert into SYSTEM.PROFEMP (ID)
+values (670486299);
+
+insert into SYSTEM.PROFEMP (ID)
+values (158125556);
+
+insert into SYSTEM.PROFEMP (ID)
+values (544770918);
+
+insert into SYSTEM.PROFEMP (ID)
+values (113012879);
+
+insert into SYSTEM.PROFEMP (ID)
+values (188513854);
+
+insert into SYSTEM.PROFEMP (ID)
+values (460205406);
+
+insert into SYSTEM.PROFEMP (ID)
+values (396671903);
+
+insert into SYSTEM.PROFEMP (ID)
+values (441543483);
+
+insert into SYSTEM.PROFEMP (ID)
+values (242331033);
+
+insert into SYSTEM.PROFEMP (ID)
+values (481333110);
+
+insert into SYSTEM.PROFEMP (ID)
+values (127289244);
+
+insert into SYSTEM.PROFEMP (ID)
+values (728627547);
+
+insert into SYSTEM.PROFEMP (ID)
+values (418613193);
+
+insert into SYSTEM.PROFEMP (ID)
+values (630604164);
+
+insert into SYSTEM.PROFEMP (ID)
+values (671418126);
+
+insert into SYSTEM.PROFEMP (ID)
+values (542589219);
+
+insert into SYSTEM.PROFEMP (ID)
+values (780571907);
+
+insert into SYSTEM.PROFEMP (ID)
+values (431404507);
+
+insert into SYSTEM.PROFEMP (ID)
+values (455887097);
+
+insert into SYSTEM.PROFEMP (ID)
+values (576206737);
+
+insert into SYSTEM.PROFEMP (ID)
+values (669667091);
+
+insert into SYSTEM.PROFEMP (ID)
+values (716401271);
+
+insert into SYSTEM.PROFEMP (ID)
+values (749553545);
+
+insert into SYSTEM.PROFEMP (ID)
+values (449973838);
+
+insert into SYSTEM.PROFEMP (ID)
+values (970632356);
+
+insert into SYSTEM.PROFEMP (ID)
+values (446737583);
+
+insert into SYSTEM.PROFEMP (ID)
+values (382646510);
+
+insert into SYSTEM.PROFEMP (ID)
+values (523698495);
+
+insert into SYSTEM.PROFEMP (ID)
+values (141263643);
+
+insert into SYSTEM.PROFEMP (ID)
+values (467277244);
+
+insert into SYSTEM.PROFEMP (ID)
+values (553461609);
+
+insert into SYSTEM.PROFEMP (ID)
+values (654709433);
+
+insert into SYSTEM.PROFEMP (ID)
+values (967391561);
+
+insert into SYSTEM.PROFEMP (ID)
+values (685710145);
+
+insert into SYSTEM.PROFEMP (ID)
+values (860623465);
+
+insert into SYSTEM.PROFEMP (ID)
+values (918280260);
+
+insert into SYSTEM.PROFEMP (ID)
+values (145057648);
+
+insert into SYSTEM.PROFEMP (ID)
+values (748936045);
+
+insert into SYSTEM.PROFEMP (ID)
+values (201027879);
+
+insert into SYSTEM.PROFEMP (ID)
+values (875560193);
+
+insert into SYSTEM.PROFEMP (ID)
+values (409478549);
+
+insert into SYSTEM.PROFEMP (ID)
+values (272577592);
+
+insert into SYSTEM.PROFEMP (ID)
+values (198011995);
+
+insert into SYSTEM.PROFEMP (ID)
+values (993643670);
+
+insert into SYSTEM.PROFEMP (ID)
+values (309479769);
+
+insert into SYSTEM.PROFEMP (ID)
+values (482839504);
+
+insert into SYSTEM.PROFEMP (ID)
+values (406974798);
+
+insert into SYSTEM.PROFEMP (ID)
+values (558986148);
+
+insert into SYSTEM.PROFEMP (ID)
+values (453429882);
+
+insert into SYSTEM.PROFEMP (ID)
+values (961017958);
+
+insert into SYSTEM.PROFEMP (ID)
+values (658952662);
+
+insert into SYSTEM.PROFEMP (ID)
+values (558499469);
+
+insert into SYSTEM.PROFEMP (ID)
+values (433300247);
+
+insert into SYSTEM.PROFEMP (ID)
+values (587781102);
+
+insert into SYSTEM.PROFEMP (ID)
+values (386639566);
+
+insert into SYSTEM.PROFEMP (ID)
+values (389323430);
+
+insert into SYSTEM.PROFEMP (ID)
+values (519891095);
+
+insert into SYSTEM.PROFEMP (ID)
+values (389323430);
+
+insert into SYSTEM.PROFEMP (ID)
+values (761977746);
+
+insert into SYSTEM.PROFEMP (ID)
+values (292934456);
+
+insert into SYSTEM.PROFEMP (ID)
+values (683940317);
+
+insert into SYSTEM.PROFEMP (ID)
+values (827714513);
+
+insert into SYSTEM.PROFEMP (ID)
+values (103306043);
+
+insert into SYSTEM.PROFEMP (ID)
+values (150863497);
+
+insert into SYSTEM.PROFEMP (ID)
+values (340618106);
+
+insert into SYSTEM.PROFEMP (ID)
+values (669667091);
+
+insert into SYSTEM.PROFEMP (ID)
+values (726387742);
+
+insert into SYSTEM.PROFEMP (ID)
+values (140265800);
+
+insert into SYSTEM.PROFEMP (ID)
+values (494972656);
+
+insert into SYSTEM.PROFEMP (ID)
+values (483177605);
+
+insert into SYSTEM.PROFEMP (ID)
+values (139133141);
+
+insert into SYSTEM.PROFEMP (ID)
+values (603504455);
+
+insert into SYSTEM.PROFEMP (ID)
+values (877302458);
+
+insert into SYSTEM.PROFEMP (ID)
+values (320749606);
+
+insert into SYSTEM.PROFEMP (ID)
+values (843652590);
+
+insert into SYSTEM.PROFEMP (ID)
+values (811723654);
+
+insert into SYSTEM.PROFEMP (ID)
+values (391043435);
+
+insert into SYSTEM.PROFEMP (ID)
+values (193814410);
+
+insert into SYSTEM.PROFEMP (ID)
+values (190536629);
+
+insert into SYSTEM.PROFEMP (ID)
+values (706452077);
+
+insert into SYSTEM.PROFEMP (ID)
+values (715569577);
+
+insert into SYSTEM.PROFEMP (ID)
+values (559115793);
+
+insert into SYSTEM.PROFEMP (ID)
+values (193814410);
+
+insert into SYSTEM.PROFEMP (ID)
+values (554692982);
+
+insert into SYSTEM.PROFEMP (ID)
+values (174900610);
+
+insert into SYSTEM.PROFEMP (ID)
+values (625812936);
+
+insert into SYSTEM.PROFEMP (ID)
+values (452790747);
+
+insert into SYSTEM.PROFEMP (ID)
+values (581624203);
+
+insert into SYSTEM.PROFEMP (ID)
+values (737466972);
+
+insert into SYSTEM.PROFEMP (ID)
+values (544185529);
+
+insert into SYSTEM.PROFEMP (ID)
+values (651508210);
+
+insert into SYSTEM.PROFEMP (ID)
+values (916014048);
+
+insert into SYSTEM.PROFEMP (ID)
+values (701120691);
+
+insert into SYSTEM.PROFEMP (ID)
+values (905701045);
+
+insert into SYSTEM.PROFEMP (ID)
+values (712693306);
+
+insert into SYSTEM.PROFEMP (ID)
+values (138456428);
+
+insert into SYSTEM.PROFEMP (ID)
+values (773133382);
+
+insert into SYSTEM.PROFEMP (ID)
+values (202601345);
+
+insert into SYSTEM.PROFEMP (ID)
+values (383347242);
+
+insert into SYSTEM.PROFEMP (ID)
+values (385462243);
+
+insert into SYSTEM.PROFEMP (ID)
+values (851271282);
+
+insert into SYSTEM.PROFEMP (ID)
+values (420504508);
+
+insert into SYSTEM.PROFEMP (ID)
+values (743719559);
+
+insert into SYSTEM.PROFEMP (ID)
+values (679371335);
+
+insert into SYSTEM.PROFEMP (ID)
+values (363925309);
+
+insert into SYSTEM.PROFEMP (ID)
+values (294114777);
+
+insert into SYSTEM.PROFEMP (ID)
+values (396671903);
+
+insert into SYSTEM.PROFEMP (ID)
+values (473828621);
+
+insert into SYSTEM.PROFEMP (ID)
+values (269382043);
+
+insert into SYSTEM.PROFEMP (ID)
+values (743090396);
+
+insert into SYSTEM.PROFEMP (ID)
+values (650319988);
+
+insert into SYSTEM.PROFEMP (ID)
+values (402713784);
+
+insert into SYSTEM.PROFEMP (ID)
+values (783475695);
+
+insert into SYSTEM.PROFEMP (ID)
+values (114509967);
+
+insert into SYSTEM.PROFEMP (ID)
+values (361626514);
+
+insert into SYSTEM.PROFEMP (ID)
+values (303842916);
+
+insert into SYSTEM.PROFEMP (ID)
+values (951787577);
+
+insert into SYSTEM.PROFEMP (ID)
+values (230927098);
+
+insert into SYSTEM.PROFEMP (ID)
+values (349784891);
+
+insert into SYSTEM.PROFEMP (ID)
+values (742907963);
+
+insert into SYSTEM.PROFEMP (ID)
+values (867973416);
+
+insert into SYSTEM.PROFEMP (ID)
+values (300377869);
+
+insert into SYSTEM.PROFEMP (ID)
+values (867955360);
+
+insert into SYSTEM.PROFEMP (ID)
+values (331997458);
+
+insert into SYSTEM.PROFEMP (ID)
+values (522192233);
+
+insert into SYSTEM.PROFEMP (ID)
+values (559829840);
+
+insert into SYSTEM.PROFEMP (ID)
+values (403249356);
+
+insert into SYSTEM.PROFEMP (ID)
+values (198011995);
+
+insert into SYSTEM.PROFEMP (ID)
+values (247161269);
+
+insert into SYSTEM.PROFEMP (ID)
+values (225400800);
+
+insert into SYSTEM.PROFEMP (ID)
+values (869885734);
+
+insert into SYSTEM.PROFEMP (ID)
+values (522192233);
+
+insert into SYSTEM.PROFEMP (ID)
+values (748587451);
+
+insert into SYSTEM.PROFEMP (ID)
+values (491709643);
+
+insert into SYSTEM.PROFEMP (ID)
+values (852787463);
+
+insert into SYSTEM.PROFEMP (ID)
+values (748587451);
+
+insert into SYSTEM.PROFEMP (ID)
+values (834559675);
+
+insert into SYSTEM.PROFEMP (ID)
+values (643626452);
+
+insert into SYSTEM.PROFEMP (ID)
+values (861772188);
+
+insert into SYSTEM.PROFEMP (ID)
+values (238369139);
+
+insert into SYSTEM.PROFEMP (ID)
+values (296537579);
+
+insert into SYSTEM.PROFEMP (ID)
+values (172660024);
+
+insert into SYSTEM.PROFEMP (ID)
+values (400923813);
+
+insert into SYSTEM.PROFEMP (ID)
+values (376489608);
+
+insert into SYSTEM.PROFEMP (ID)
+values (572241147);
+
+insert into SYSTEM.PROFEMP (ID)
+values (224289578);
+
+insert into SYSTEM.PROFEMP (ID)
+values (862724973);
+
+insert into SYSTEM.PROFEMP (ID)
+values (640996297);
+
+insert into SYSTEM.PROFEMP (ID)
+values (667089564);
+
+insert into SYSTEM.PROFEMP (ID)
+values (790547833);
+
+insert into SYSTEM.PROFEMP (ID)
+values (702734794);
+
+insert into SYSTEM.PROFEMP (ID)
+values (508272693);
+
+insert into SYSTEM.PROFEMP (ID)
+values (636754601);
+
+insert into SYSTEM.PROFEMP (ID)
+values (914586093);
+
+insert into SYSTEM.PROFEMP (ID)
+values (875952935);
+
+insert into SYSTEM.PROFEMP (ID)
+values (675511711);
+
+insert into SYSTEM.PROFEMP (ID)
+values (682399103);
+
+insert into SYSTEM.PROFEMP (ID)
+values (821100483);
+
+insert into SYSTEM.PROFEMP (ID)
+values (950705566);
+
+insert into SYSTEM.PROFEMP (ID)
+values (399138761);
+
+insert into SYSTEM.PROFEMP (ID)
+values (170548339);
+
+insert into SYSTEM.PROFEMP (ID)
+values (304636867);
+
+insert into SYSTEM.PROFEMP (ID)
+values (523698495);
+
+insert into SYSTEM.PROFEMP (ID)
+values (117071632);
+
+insert into SYSTEM.PROFEMP (ID)
+values (762171271);
+
+insert into SYSTEM.PROFEMP (ID)
+values (703870183);
+
+insert into SYSTEM.PROFEMP (ID)
+values (175230934);
+
+insert into SYSTEM.PROFEMP (ID)
+values (901161344);
+
+insert into SYSTEM.PROFEMP (ID)
+values (813536083);
+
+insert into SYSTEM.PROFEMP (ID)
+values (500955521);
+
+insert into SYSTEM.PROFEMP (ID)
+values (671246474);
+
+insert into SYSTEM.PROFEMP (ID)
+values (376489608);
+
+insert into SYSTEM.PROFEMP (ID)
+values (329740922);
+
+insert into SYSTEM.PROFEMP (ID)
+values (959885530);
+
+insert into SYSTEM.PROFEMP (ID)
+values (961017958);
+
+insert into SYSTEM.PROFEMP (ID)
+values (141263643);
+
+insert into SYSTEM.PROFEMP (ID)
+values (678595222);
+
+insert into SYSTEM.PROFEMP (ID)
+values (855566833);
+
+insert into SYSTEM.PROFEMP (ID)
+values (984730264);
+
+insert into SYSTEM.PROFEMP (ID)
+values (735341704);
+
+insert into SYSTEM.PROFEMP (ID)
+values (777069441);
+
+insert into SYSTEM.PROFEMP (ID)
+values (255012469);
+
+insert into SYSTEM.PROFEMP (ID)
+values (678244313);
+
+insert into SYSTEM.PROFEMP (ID)
+values (376489608);
+
+insert into SYSTEM.PROFEMP (ID)
+values (126344757);
+
+insert into SYSTEM.PROFEMP (ID)
+values (372917617);
+
+insert into SYSTEM.PROFEMP (ID)
+values (674733809);
+
+insert into SYSTEM.PROFEMP (ID)
+values (215244999);
+
+insert into SYSTEM.PROFEMP (ID)
+values (168326122);
+
+insert into SYSTEM.PROFEMP (ID)
+values (133563225);
+
+insert into SYSTEM.PROFEMP (ID)
+values (919795288);
+
+insert into SYSTEM.PROFEMP (ID)
+values (196623412);
+
+insert into SYSTEM.PROFEMP (ID)
+values (587781102);
+
+insert into SYSTEM.PROFEMP (ID)
+values (741921651);
+
+insert into SYSTEM.PROFEMP (ID)
+values (138789569);
+
+insert into SYSTEM.PROFEMP (ID)
+values (312965468);
+
+insert into SYSTEM.PROFEMP (ID)
+values (835549283);
+
+insert into SYSTEM.PROFEMP (ID)
+values (676322518);
+
+insert into SYSTEM.PROFEMP (ID)
+values (783475695);
+
+insert into SYSTEM.PROFEMP (ID)
+values (947091311);
+
+insert into SYSTEM.PROFEMP (ID)
+values (325472778);
+
+insert into SYSTEM.PROFEMP (ID)
+values (203220545);
+
+insert into SYSTEM.PROFEMP (ID)
+values (930386082);
+
+insert into SYSTEM.PROFEMP (ID)
+values (551897883);
+
+insert into SYSTEM.PROFEMP (ID)
+values (712883247);
+
+insert into SYSTEM.PROFEMP (ID)
+values (439701854);
+
+insert into SYSTEM.PROFEMP (ID)
+values (420504508);
+
+insert into SYSTEM.PROFEMP (ID)
+values (651508210);
+
+insert into SYSTEM.PROFEMP (ID)
+values (808481547);
+
+insert into SYSTEM.PROFEMP (ID)
+values (208521758);
+
+insert into SYSTEM.PROFEMP (ID)
+values (757691225);
+
+insert into SYSTEM.PROFEMP (ID)
+values (201027879);
+
+insert into SYSTEM.PROFEMP (ID)
+values (391050550);
+
+insert into SYSTEM.PROFEMP (ID)
+values (114509967);
+
+insert into SYSTEM.PROFEMP (ID)
+values (921471254);
+
+insert into SYSTEM.PROFEMP (ID)
+values (789601499);
+
+commit;
+
